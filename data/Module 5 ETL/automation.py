f7497cdef57be2a16f6c72aaf95f9e235e3ca779
SannyGarin <141878054+SannyGarin@users.noreply.github.com>
2024-11-14 17:55:50 +0800

    Update automation.py

--- data/Module 5 ETL/automation.py
+++ data/Module 5 ETL/automation.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 # connect to database
 # You can get the Hostname and Password from the connection information section of Mysql 
-connection = mysql.connector.connect(user='root', password='R10Iv6WpmjCdUYWGP5V2e2KV',host='172.21.38.90',database='Sales')
+connection = mysql.connector.connect(user='root', password='b2wowFcIsrW6J8BhmgKg9LDj',host='172.21.116.31',database='Sales')
 
 # create cursor
 
@@ -16,12 +16,11 @@ import psycopg2
 
 # connectction details
 
-dsn_hostname = '172.21.222.205'
+dsn_hostname = '172.21.96.114'
 dsn_user='postgres'        # e.g. "abc12345"
-dsn_pwd ='ysgYqSFBLXuOGB4UqhbQnqIb'      # e.g. "7dBZ3wWt9XN6$o0J"
+dsn_pwd ='w8h8hCKBKE80lk21vneqAzAN'      # e.g. "7dBZ3wWt9XN6$o0J"
 dsn_port ="5432"                # e.g. "50000" 
 dsn_database ="Sales"           # i.e. "BLUDB"
-
 
 # create connection
 
@@ -54,7 +53,6 @@ def get_last_rowid():
         # print(row)
         return  row[0]
 
-
 last_row_id = get_last_rowid()
 
 print("Last row id on production datawarehouse = ", last_row_id)
@@ -64,7 +62,7 @@ print("Last row id on production datawarehouse = ", last_row_id)
 
 def get_latest_records(rowid):
     records = []
-    SQL = f"SELECT * FROM sales_data WHERE ROWID > {rowid}"	
+    SQL = f"SELECT * FROM sales_data WHERE ROWID > {rowid}" 
     cursor_mysql.execute(SQL)
     for row in cursor_mysql.fetchall():
         records.append(row)
@@ -72,18 +70,23 @@ def get_latest_records(rowid):
     
 new_records = get_latest_records(last_row_id)
 
+
 print("New rows on staging datawarehouse = ", len(new_records))
-     
 
 # Insert the additional records from MySQL into DB2 or PostgreSql data warehouse.
 # The function insert_records must insert all the records passed to it into the sales_data table in IBM DB2 database or PostgreSql.
 
+
+
+
 def insert_records(records):
     for record in records:
-	    cursor.execute(f"INSERT INTO  SALES_DATA(rowid,product_id,customer_id,quantity) VALUES{record} ON CONFLICT (rowid) DO NOTHING;");
-    return conn.commit
-    
+        SQL="INSERT INTO  public.sales_data(rowid,product_id,customer_id,quantity) values(%s,%s,%s,%s) ON CONFLICT (rowid) DO NOTHING;" 
+        cursor.execute(SQL,record);
+        conn.commit()
+        
 insert_records(new_records)
+
 print("New rows inserted into production datawarehouse = ", len(new_records))
 
 # disconnect from mysql warehouse
@@ -91,7 +94,3 @@ connection.close()
 # disconnect from DB2 or PostgreSql data warehouse 
 conn.close()
 # End of program
-
-
-
-
